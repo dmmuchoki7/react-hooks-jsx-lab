324594318633b6e7b49035cc263c23fbaefb696f
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-11-08 10:42:15 +0300

    completed task

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import {name, city} from "../data/data.js";
+
+// Style function
+const HStyle = {
+  color: 'firebrick'
+};
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+   <h1 style={HStyle}>{name} is a Web Developer from {city}</h1> 
+  </div>;
 }
 
 export default Home;
